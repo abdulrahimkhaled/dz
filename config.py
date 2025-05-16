6c95c6b3862501accfdc235a1ab8395897d21d99
abdulrahimkhaled <141029291+abdulrahimkhaled@users.noreply.github.com>
2025-05-17 02:51:16 +0300

    config.py

--- config.py
+++ config.py
@@ -2,4 +2,4 @@ TOKEN = "7804028306:AAFSpq_x_4i2-zzp8aGr4rglMrXa2rcbjU0"
 
 DB_URI = "postgresql://postgres:dzireedzireE01@@db.yejjnjtxqeguwelcrngz.supabase.co:5432/postgres"
 
-BASE_URL = "https://your-bot-service.onrender.com"
+BASE_URL = "https://dz-fj3j.onrender.com"
