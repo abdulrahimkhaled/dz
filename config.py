9ef4ce4b96803e3646c115ddb2ce1b1890e17bca
abdulrahimkhaled <141029291+abdulrahimkhaled@users.noreply.github.com>
2025-05-17 20:49:26 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,3 @@
-TOKEN = "7804028306:AAFSpq_x_4i2-zzp8aGr4rglMrXa2rcbjU0"
-
 DB_URI = "postgresql://postgres.yejjnjtxqeguwelcrngz:dzireedzireE01@aws-0-eu-central-1.pooler.supabase.com:6543/postgres"
 
 BASE_URL = "https://dz-fj3j.onrender.com"
