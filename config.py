79b793c55c90f7499d649498a6129f6e4f2a28e5
abdulrahimkhaled <141029291+abdulrahimkhaled@users.noreply.github.com>
2025-05-17 16:44:19 +0300

    config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 TOKEN = "7804028306:AAFSpq_x_4i2-zzp8aGr4rglMrXa2rcbjU0"
 
-DB_URI = "postgresql://postgres:dzireedzireE01@@db.yejjnjtxqeguwelcrngz.supabase.co:5432/postgres"
+DB_URI = "postgresql://postgres.yejjnjtxqeguwelcrngz:dzireedzireE01@aws-0-eu-central-1.pooler.supabase.com:6543/postgres"
 
 BASE_URL = "https://dz-fj3j.onrender.com"
